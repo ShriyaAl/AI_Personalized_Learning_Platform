f0fdb8c5156baf8332d2e9d88c2865743bd53f37
ShriyaAl <alladishriya05@gmail.com>
2026-02-20 10:20:18 +0530

    Set up Supabase too

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import TestRouter from './routes/testRoute.js';
+import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+
+import { createClient } from '@supabase/supabase-js';
+
+dotenv.config();
+const supabase = createClient(process.env.DATABASE_URL,process.env.DATABASE_KEY);
 
 const app = express();
 
@@ -17,6 +23,19 @@ app.use('/api/test', TestRouter);
 app.get('/', (req, res) => {
     res.send("Backend set up");
 });
+
+// Get all articles
+
+app.get("/articles", async (_, response) => {
+  try {
+    const { data, error } = await supabase.from("Posts").select();
+    console.log(data);
+    return response.send(data);
+  } catch (error) {
+    return response.send({ error });
+  }
+});
+
 
 app.listen(3000, () => {
     console.log(`🚀 Backend is running on http://localhost:3000`);
